d636ebe66b478fa6d91b301a6b862546038acbb2
lehuy16 <cwalklhuy@gmail.com>
2023-03-03 13:35:16 +0700

    Refix Test

--- starter/catpoint-parent/security-service/src/main/java/module-info.java
+++ starter/catpoint-parent/security-service/src/main/java/module-info.java
@@ -2,8 +2,9 @@ open module com.udacity.catpoint.security {
     requires com.udacity.catpoint.image;
     requires miglayout;
     requires java.desktop;
-    requires java.prefs;
     requires com.google.common;
     requires com.google.gson;
     requires java.sql;
+    requires java.prefs;
+
 }
